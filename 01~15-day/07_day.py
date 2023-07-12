4eb0dd44c2bc620b87225866858e43293f282ca2
Benjamin <bjzhou@outlook.com>
2023-07-12 23:41:42 +0800

    fix 07_day: str, list

--- 01~15-day/07_day.py
+++ 01~15-day/07_day.py
@@ -40,3 +40,58 @@ print(str1[-3::-1]) # 4321cba
 print(str1[1::-3])  # b
 print(str1[-1::-3]) # 63c
 
+print()
+
+str1 = 'hello, world!'
+print(len(str1))                # 13
+print(str1.capitalize())        # Hello, world!
+print(str1.title())             # Hello, World!
+print(str1.upper())             # HELLO, WORLD!
+print(str1.find('or'))          # 8
+print(str1.find('shit'))        # -1（未找到）
+print(str1.startswith('he'))    # True
+print(str1.startswith('al'))    # False
+print(str1.center(50, '*'))     # 指定宽度居中，*补齐
+print(str1.rjust(50, ' '))      # 制定宽度居右， 补齐
+print(str1.ljust(50, '-'))      # 制定宽度居左，-补齐
+
+str2 = 'abc123456'
+print(str2.isdigit())           # False 是否数字字符串
+print(str2.isalpha())           # False 是否字母字符串
+print(str2.isalnum())           # True  是否数字字母字符串
+
+str3 = '  jackfrued@126.com '
+print(str3.strip())             # jackfrued@126.com 去除空格
+
+print()
+
+a, b = 5, 10
+print('%d * %d = %d' % (a, b, a * b))
+print('{0} * {1} = {2}'.format(a, b, a * b))
+print(f'{a} * {b} = {a * b}')
+
+print()
+
+# list
+list1 = [2, 3, 4, 5, 7, 11, 13, 17, 19]
+print(list1)                # [2, 3, 4, 5, 7, 11, 13, 17, 19]
+list2 = ['Scofield']        
+print(list2 * 3)            # ['Scofield', 'Scofield', 'Scofield']
+print(len(list1))           # 9
+print(list1[0])             # 2
+print(list1[-1])            # 19
+# print(list1[9])             # IndexError: list index out of range
+list1[0] = 1                # 赋值
+print(list1)                # [1, 3, 4, 5, 7, 11, 13, 17, 19]
+
+# 下标遍历
+for i in range(len(list1)):
+    print(list1[i])    
+
+# for...in 遍历
+for index in list1:
+    print(index)
+    
+# enumerate: 遍历索引和值
+for index, value in enumerate(list1):
+    print(index, value)
