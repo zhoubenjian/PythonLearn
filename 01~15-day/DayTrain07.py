f7e7701dc959bfe75c7c0e641c3784831946d273
Benjamin <bjzhou@outlook.com>
2023-07-27 22:57:33 +0800

    add DayTrain07

--- 01~15-day/DayTrain07.py
+++ 01~15-day/DayTrain07.py
@@ -39,5 +39,29 @@ def get_suffix(filename, has_dot = False):
     
     
 # 练习4：设计一个函数返回传入的列表中最大和第二大的元素的值。
+def max2(x):
+    m1, m2 = (x[0], x[1]) if x[0] > x[1] else (x[1], x[0])
+    for i in range(2, len(x)):
+        if x[i] > m1:
+            m2 = m1
+            m1 = x[i]
+        elif x[i] > m2:
+            m2 = x[i]
+    return m1, m2
+      
+            
+# 练习5：计算指定的年月日是这一年的第几天。
+def is_leap_year(year):
+    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+
+def which_day(year, month, day):
+    day_num = [
+        [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31], 
+        [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        ]
+    total = 0
+    for i in range(month - 1):
+        total += day_num[is_leap_year(year)][i]
+    return (total + day)
 
     
